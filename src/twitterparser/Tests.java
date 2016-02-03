39ea43346e480c734ea59b4835e61520fea4ad39
sarahmehmedi <sarahmehmedi@gmail.com>
2016-02-02 18:43:45 -0600

    updated variables and added new tests

--- src/twitterparser/Tests.java
+++ src/twitterparser/Tests.java
@@ -10,13 +10,14 @@ import org.junit.Test;
 
 public class Tests {
 	
-	String firstTweet = "Test #hashtag";
+	String firstTweet = "Hello I #am testing #hashtags @sarah and www.sarahmehmedi.com";
+	String secondTweet = "I am @Testing mentions hashtag";
 	
 	@Test
 	public void testingHashtags() {
 
 		@SuppressWarnings("resource")
-		Scanner hashtag = new Scanner("Hello I #am testing #hashtags @sarah and www.sarahmehmedi.com");
+		Scanner hashtag = new Scanner(firstTweet);
 		String tweets = hashtag.nextLine();
 		
 		TwitterParser tweet = TwitterParser.getTweet(tweets.toLowerCase());
@@ -24,20 +25,24 @@ public class Tests {
 		testingHashtags.add("#am");
 		testingHashtags.add("#hashtags");
 		
-		ArrayList<String> testingMentions = new ArrayList <String>();
-		testingMentions.add("@sarah");
-		
 		ArrayList<String> testingWebsites = new ArrayList <String>();
 		testingWebsites.add("www.sarahmehmedi.com");
 		
 		assertEquals(testingHashtags, tweet.getUniqueHashtags());
-		assertEquals(testingMentions, tweet.getUniqueMentions());
 		assertEquals(testingWebsites, tweet.getUniqueWebsites());
 	}
 	
 	@Test
 	public void testingMentions(){
+		@SuppressWarnings("resource")
+		Scanner mention = new Scanner(secondTweet);
+		String tweets2 = mention.nextLine();
 		
+		TwitterParser tweet2 = TwitterParser.getTweet(tweets2.toLowerCase());
+		ArrayList<String> testingMentions = new ArrayList <String>();
+		testingMentions.add("@testing");
+		
+		assertEquals(testingMentions, tweet2.getUniqueMentions());
 	}
 
 }
