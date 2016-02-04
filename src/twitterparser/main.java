ddf6e45515e69ec286e385ec2b1f80fa123e7bd7
sarahmehmedi <sarahmehmedi@gmail.com>
2016-02-04 15:26:49 -0600

    updated readinginputs

--- src/twitterparser/main.java
+++ src/twitterparser/main.java
@@ -6,18 +6,26 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		@SuppressWarnings("resource")
 		Scanner tweetText = new Scanner(System.in);
+		readInput(tweetText);
+	}
+	private static void readInput(Scanner in){
 		System.out.println("Please enter a tweet (preferably under 140 characters): ");
-//		Scanner tweetText = new Scanner("Hello I #am testing #hashtags @sarah and www.sarahmehmedi.com");
-		String tweets = tweetText.nextLine();
-		
-		TwitterParser tweet = TwitterParser.getTweet(tweets.toLowerCase());
-		
-		System.out.println("Hashtags: " + tweet.getUniqueHashtags());
-		System.out.println("Mentions: " + tweet.getUniqueMentions());
-		System.out.println("Websites: " + tweet.getUniqueWebsites());
+		String tweets = in.nextLine();
+		if(tweets.length() <= 140){
+
+			TwitterParser tweet = TwitterParser.getTweet(tweets.toLowerCase());
+
+			System.out.println("Hashtags: " + tweet.getUniqueHashtags());
+			System.out.println("Mentions: " + tweet.getUniqueMentions());
+			System.out.println("Websites: " + tweet.getUniqueWebsites());
+		}
+		else{
+			System.out.println("Longer than 140 characters...");
+			readInput(in);
+		}
 	}
 }
+
 
 
