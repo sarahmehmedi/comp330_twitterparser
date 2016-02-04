07d49fa70258e229715fd1ac10d2ceabc0470623
sarahmehmedi <sarahmehmedi@gmail.com>
2016-02-04 14:57:22 -0600

    finished cleaning

--- src/twitterparser/main.java
+++ src/twitterparser/main.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
+		
 		@SuppressWarnings("resource")
-		Scanner hashtag = new Scanner(System.in);
-		System.out.println("Please enter a line of text: ");
-//		Scanner hashtag = new Scanner("Hello I #am testing #hashtags @sarah and www.sarahmehmedi.com");
-		String tweets = hashtag.nextLine();
+		Scanner tweetText = new Scanner(System.in);
+		System.out.println("Please enter a tweet (preferably under 140 characters): ");
+//		Scanner tweetText = new Scanner("Hello I #am testing #hashtags @sarah and www.sarahmehmedi.com");
+		String tweets = tweetText.nextLine();
 		
 		TwitterParser tweet = TwitterParser.getTweet(tweets.toLowerCase());
 		
 		System.out.println("Hashtags: " + tweet.getUniqueHashtags());
 		System.out.println("Mentions: " + tweet.getUniqueMentions());
 		System.out.println("Websites: " + tweet.getUniqueWebsites());
-		
 	}
 }
 
